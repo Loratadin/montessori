8916f34742011a58515b47161966a1dc4f1c7e8e
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-03-29 15:09:02 -0500

    Add java script file for flex panel gallery

---
+++ js/flex-panels-gallery.js
@@ -0,0 +1,5 @@
+const panels = document.querySelectorAll('.panels');
+function toggleOpen() {
+ this.classList.toggle('open');
+}
+panels.forEach(panel=>panel.addEventListener('click', toggleOpen));
