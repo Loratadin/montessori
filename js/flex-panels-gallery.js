e8cf1a5ac0ae75740140fc7977c5f244453332a4
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-03-29 15:18:45 -0500

    Add class active in java script

--- js/flex-panels-gallery.js
+++ js/flex-panels-gallery.js
@@ -1,5 +1,6 @@
-const panels = document.querySelectorAll('.panels');
+const panels = document.querySelectorAll('.panel');
 function toggleOpen() {
  this.classList.toggle('open');
 }
 panels.forEach(panel=>panel.addEventListener('click', toggleOpen));
+panels.forEach(panel=>panel.addEventListener('transitionend', toggleActive));
