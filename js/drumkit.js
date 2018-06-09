9446bfed284ab9223eb16831ea0cd90924abfba4
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-09 13:19:00 -0500

    Add enable and disable drumkit function

--- js/drumkit.js
+++ js/drumkit.js
@@ -1,17 +1,62 @@
-window.addEventListener('keydown', function(e) {
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-if(!audio) return; //stop the function from running all together
-audio.currentTime = 0; //rewind to the start when we press the same key quickly
-audio.play();
-key.classList.add('playing'); //add a class with transition effects
-});
+function drumkitHandler(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop the function from running all together
+    audio.currentTime = 0; //rewind to the start when we press the same key quickly
+    audio.play();
+    key.classList.add('playing'); //add a class with transition effects
+}
 
 //Set up end for transition effects
-
 function removeTransition(e) {
  if(e.propertyName !== 'transform') return; // skip it if it's not a transform
  this.classList.remove('playing');
 } 
 const keys = document.querySelectorAll('.key'); 
-keys.forEach( key => key.addEventListener('transitionend', removeTransition));
+keys.forEach( key => key.addEventListener('transitionend', removeTransition)); 
+
+function enableDrumkit() {
+    window.addEventListener('keydown', drumkitHandler(e));
+}
+
+function disableDrumkit() {
+    window.removeEventListener('keydown', drumkitHandler(e));
+}
+
+//Version 1
+// var  section = document.getElementById('letters');
+// // var top = section.offsetTop;
+// // var bottom = section.offsetBottom;
+// var box = section.offsetHeight;
+// window.addEventListener("scroll", function() {
+//     //if (window.pageYOffset <= top || window.pageYOffset >= bottom) {
+//      if (window.pageYOffset <= box || window.pageYOffset >= box) {
+//      enableDrumkit()
+//     } else {
+//      disableDrumkit()
+//     }
+// });
+
+//Version 2
+// var  section = document.getElementById('letters');
+// var top = section.offsetTop;
+// var bottom = section.offsetBottom;
+// window.addEventListener("scroll", function() {
+//     if (window.pageYOffset <= top || window.pageYOffset >= bottom) {
+//      enableDrumkit()
+//     } else {
+//      disableDrumkit()
+//     }
+// });
+
+//Version 3
+// var  section = document.getElementById('letters');
+// var box = section.offsetHeight
+// window.addEventListener("scroll", function(e) {
+//      let box = section.offsetHeight;
+//      if (window.pageYOffset >= box.top) {
+//      enableDrumkit()
+//     } else {
+//      disableDrumkit()
+//     }
+// });
