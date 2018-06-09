1edac9472e418e31d1f1e26d096d4f724d4ab436
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-06-09 11:02:52 -0500

    Rewrite JavaScript for navigation

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,14 +1,13 @@
 var  nav = document.getElementById('nav');
       
-      window.onscroll = function(){
+window.addEventListener("scroll", function(e) {
+	if (window.pageYOffset >100) {
 
-      	if (window.pageYOffset >100) {
-
-        nav.style.background = "rgba(103, 80, 121, 0.8)";
-      		nav.style.boxShadow = "0px 4px 2px grey";
-      	}
-      	else{
-      		nav.style.background = "transparent";
-      		nav.style.boxShadow = "none";
-      	}
+		nav.style.background = "rgba(103, 80, 121, 0.8)";
+		nav.style.boxShadow = "0px 4px 2px grey";
 	}
+	else{
+		nav.style.background = "transparent";
+		nav.style.boxShadow = "none";
+	}
+});
