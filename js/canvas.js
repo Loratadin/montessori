eb125a2593cbd8dd45a9959373bb199893be5ffa
Tatsiana Shulha <taniaissohappy@gmail.com>
2018-09-18 11:18:22 -0500

    Set up some context properties in javaScript

--- js/canvas.js
+++ js/canvas.js
@@ -1,2 +1,7 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d'); //need context for drawing, 3d is used for video games
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
